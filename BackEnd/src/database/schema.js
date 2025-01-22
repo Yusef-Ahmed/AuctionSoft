1bdb224e65585df382448b142ab0b8969fb6e21e
Youssef Ahmed <yousseffcai@gmail.com>
2025-01-22 18:04:58 +0200

    Finishing the schema

--- BackEnd/src/database/schema.js
+++ BackEnd/src/database/schema.js
@@ -1,21 +1,33 @@
-const { bigint, mysqlTable, timestamp, varchar } = require("drizzle-orm/mysql-core");
+const {
+  bigint,
+  mysqlTable,
+  varchar,
+  decimal,
+  serial,
+  tinyint,
+  timestamp,
+} = require("drizzle-orm/mysql-core");
 
 exports.users = mysqlTable("users", {
-  id: bigint("id", { mode: "number", unsigned: true })
-    .autoincrement()
-    .primaryKey(),
-  name: varchar("name", { length: 256 }).notNull(),
-  email: varchar("email", { length: 256 }).notNull().unique(),
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-  updatedAt: timestamp("updated_at").defaultNow().onUpdateNow().notNull(),
+  id: serial().primaryKey(),
+  name: varchar({ length: 256 }).notNull().unique(),
+  email: varchar({ length: 256 }).notNull().unique(),
+  password: varchar({ length: 256 }).notNull(),
 });
 
 exports.products = mysqlTable("products", {
-  id: bigint("id", { mode: "number", unsigned: true })
-    .autoincrement()
-    .primaryKey(),
-  name: varchar("name", { length: 256 }).notNull(),
-  price: bigint("price", {mode: "number"}).notNull(),
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-  updatedAt: timestamp("updated_at").defaultNow().onUpdateNow().notNull(),
+  id: serial().primaryKey(),
+  name: varchar({ length: 256 }).notNull().unique(),
+  image: varchar({ length: 256 }).notNull(),
+  price: decimal({ precision: 9, scale: 2 }).notNull(),
+  ex_date: timestamp().notNull(),
+  seller_id: bigint({ mode: 'number', unsigned: true }).references(() => this.users.id).notNull(),
+  buyer_id: bigint({ mode: 'number', unsigned: true }).references(() => this.users.id).notNull(),
+});
+
+exports.reviews = mysqlTable("reviews", {
+  id: serial().primaryKey(),
+  review: varchar({ length: 256 }).notNull().unique(),
+  rating: tinyint().notNull(),
+  seller_id: bigint({ mode: 'number', unsigned: true }).references(() => this.users.id).notNull(),
 });
--- README.md
+++ README.md
@@ -5,4 +5,7 @@ Add your application configuration to your `.env` file in the `BackEnd` folder:
 DATABASE_URL = mysql://<user>:<password>@<host>:<port>/<database>
 ```
 
+Then run `npm run db:generate` to make a `SQL` queries
+Then run `npm run db:migrate` to excequte this `SQL` queries
+
 Then type `npm start` on the `FrontEnd` folder once, and in `BackEnd` folder once more
