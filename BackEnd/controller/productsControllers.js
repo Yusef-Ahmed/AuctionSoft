5aadc69a756d28af4b9bbcbbcf9cdf2075d268b5
Youssef Ahmed <yousseffcai@gmail.com>
2025-01-23 18:25:14 +0200

    Added a new bidder

--- BackEnd/controller/productsControllers.js
+++ BackEnd/controller/productsControllers.js
@@ -1,3 +1,4 @@
+const { eq } = require("drizzle-orm");
 const { products } = require("../util/database/schema");
 const db = require("../util/database/setup");
 
@@ -9,7 +10,7 @@ exports.allProducts = async (_req, res, next) => {
     if (!err.statusCode) err.statusCode = 500;
     next(err);
   }
-}
+};
 
 exports.createProduct = async (req, res, next) => {
   const product = {
@@ -19,12 +20,53 @@ exports.createProduct = async (req, res, next) => {
     ex_date: new Date(req.body.ex_date),
     seller_id: req.userId,
   };
-  
+
   try {
     await db.insert(products).values(product);
     res.status(200).json({
       message: "Product created successfully",
-    })
+    });
+  } catch (err) {
+    if (!err.statusCode) err.statusCode = 500;
+    next(err);
+  }
+};
+
+exports.newBidder = async (req, res, next) => {
+  const productId = req.body.productId;
+  const buyerId = +req.userId;
+  const newPrice = +req.body.newPrice;
+
+  try {
+    const [product] = await db
+      .select({ seller_id: products.seller_id, price: products.price })
+      .from(products)
+      .where(eq(productId, products.id));
+
+    if (!product) {
+      const err = new Error("The product doesn't exist anymore");
+      err.statusCode = 401;
+      return next(err);
+    }
+
+    if (buyerId == product.seller_id) {
+      const err = new Error("You can't bid on you own product");
+      err.statusCode = 401;
+      return next(err);
+    }
+
+    if (newPrice <= +product.price) {
+      const err = new Error("You need to bid with a bigger price");
+      err.statusCode = 401;
+      return next(err);
+    }
+
+    await db
+      .update(products)
+      .set({ price: newPrice, buyer_id: buyerId })
+      .where(eq(productId, products.id));
+
+    res.status(200).json({message : "Updated successfully"});
   } catch (err) {
     if (!err.statusCode) err.statusCode = 500;
     next(err);
--- BackEnd/routes/productsRoutes.js
+++ BackEnd/routes/productsRoutes.js
@@ -9,4 +9,6 @@ router.get("/all", isAuth, productsControllers.allProducts);
 
 router.post("/createProduct", isAuth, productsControllers.createProduct);
 
+router.put("/newBidder", isAuth, productsControllers.newBidder);
+
 module.exports = router;
